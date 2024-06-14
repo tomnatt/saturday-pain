c6eee25e8d6c3a8765bbd1457c145c39e04b5ba3
tomnatt <github@tomnatt.com>
2024-06-14 13:27:04 +0100

    Remove test code - will delete this file soon

--- test.rb
+++ test.rb
@@ -1,20 +1,4 @@
 require 'yaml'
 
-require './lib/flickr_api'
-require './lib/photo'
-
-sat_pain_photoset_id = 72_157_711_613_641_123
-
-flickr_api = FlickrApi.new(ENV.fetch('FLICKR_USER_ID'), ENV.fetch('FLICKR_API_KEY'))
-ids = flickr_api.get_photo_ids_in_photoset(sat_pain_photoset_id)
-
-# Turn all into metadata
-ids.each_with_index do |id, i|
-  break if i > 2
-
-  photo = flickr_api.get_photo_metadata(id)
-  photo.save('images')
-end
-
 # This loads the YAML back to a Photo object
 # pic = YAML.safe_load(yaml, permitted_classes: [Photo])
